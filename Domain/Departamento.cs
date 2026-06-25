7e6c8a834f422b8250513a1c6bfc13d4bcca661f
FABIAn3RS <balsecafabian4@gmail.com>
2026-06-24 19:41:19 -0500

    commit 8

--- Domain/Departamento.cs
+++ Domain/Departamento.cs
@@ -8,3 +8,12 @@ public class Departamento
 
 
 }
+
+public interface IDepartamentoRepository
+{
+    Task<List<Departamento>> GetAllAsync();
+    Task<Departamento?> GetByIdAsync(int id);
+    Task AddAsync(Departamento departamento);
+    Task UpdateAsync(Departamento departamento);
+    Task DeleteAsync(int id);
+}
